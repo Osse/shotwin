22d6a0966b726addb4562baf754c755998a48627
Øystein Walle <oystwa@gmail.com>
2017-11-16 13:23:31 +0100

    Add flat event model with filter
    Add a model that representes the Events in a flat model, very similar
    to the PhotoModel. Also add a filter model that can filter the event
    model based on year and month.
    
    Adding this removes the need for the FilterFlatten model.

---
+++ models/datefilteredeventmodel.h
@@ -0,0 +1,30 @@
+#ifndef DATEFILTEREDEVENTMODEL_H
+#define DATEFILTEREDEVENTMODEL_H
+
+#include <QSortFilterProxyModel>
+
+class DateFilteredEventModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    DateFilteredEventModel(QObject* parent = nullptr);
+
+    bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const override;
+
+    void setFilterYear(int value);
+    void setFilterMonth(const int& year, const int& month);
+
+signals:
+    void filterChanged();
+
+private:
+    void invalidate();
+
+
+private:
+    int year = -1;
+    int month = -1;
+};
+
+#endif // DATEFILTEREDEVENTMODEL_H
